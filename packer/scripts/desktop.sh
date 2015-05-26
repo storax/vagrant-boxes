13960b53bf71af2aef152c05a2e361180b2bb0b0
David Zuber <zuber.david@gmx.de>
2015-05-26 19:01:19 +0200

    install ubuntu desktop fully

--- packer/scripts/desktop.sh
+++ packer/scripts/desktop.sh
@@ -1,8 +1,5 @@
 #!/bin/sh -eux
 
 sudo apt-get update
+sudo apt-get install -y ubuntu-desktop
 sudo apt-get upgrade -y
-
-sudo apt-get remove -y --purge xserver-xorg
-sudo apt-get install -y xserver-xorg
-sudo dpkg-reconfigure xserver-xorg
