9e350981b4f5d1fdcebe68d23ece45a6c6c97785
David Zuber <zuber.david@gmx.de>
2015-05-26 21:04:41 +0200

    install desktop without recommends
    revert back to install some packages by hand

--- packer/scripts/desktop.sh
+++ packer/scripts/desktop.sh
@@ -1,5 +1,25 @@
 #!/bin/sh -eux
 
 sudo apt-get update
-sudo apt-get install -y ubuntu-desktop
+sudo apt-get install  -y --no-install-recommends ubuntu-desktop
 sudo apt-get upgrade -y
+
+echo "Installed ubuntu desktop!"
+
+declare -a pkgs=(
+    app-install-data-partner
+    gnome-disk-utility
+    gnome-terminal
+    indicator-application
+    indicator-appmenu
+    indicator-messages
+    indicator-session
+    network-manager-gnome
+    overlay-scrollbar
+    ubuntu-system-settings
+    unity-lens-applications
+    unity-lens-files
+)
+ 
+ 
+sudo apt-get install -y ${pkgs[*]}
