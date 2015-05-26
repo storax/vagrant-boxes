e32441865698c14387369193f13634c42c092d5b
David Zuber <zuber.david@gmx.de>
2015-05-26 16:59:56 +0200

    Fix xserver-xorg bug by reinstalling it

--- packer/scripts/desktop.sh
+++ packer/scripts/desktop.sh
@@ -2,24 +2,7 @@
 
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install  -y --no-install-recommends ubuntu-desktop
-sudo apt-get upgrade -y
 
-echo "Installed ubuntu desktop!"
-
-declare -a pkgs=(
-    app-install-data-partner
-    gnome-disk-utility
-    gnome-terminal
-    indicator-application
-    indicator-appmenu
-    indicator-messages
-    indicator-session
-    network-manager-gnome
-    overlay-scrollbar
-    ubuntu-system-settings
-    unity-lens-applications
-    unity-lens-files
-)
-
-sudo apt-get install -y ${pkgs[*]}
+sudo apt-get remove -y --purge xserver-xorg
+sudo apt-get install -y xserver-xorg
+sudo dpkg-reconfigure xserver-xorg
